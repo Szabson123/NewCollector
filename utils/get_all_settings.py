69e3c5debda56833983d1cc195150fcb64ec854e
Szabson <szymonzaba18@gmail.com>
2026-06-08 09:51:22 +0200

    hotfix

--- utils/get_all_settings.py
+++ utils/get_all_settings.py
@@ -1,16 +1,17 @@
 def all_settings_to_machine(cur, comp_name):
+    # Zamieniono JOIN na LEFT JOIN dla settings i rules
     query = """
         SELECT 
             c.id AS comp_id, c.name AS comp_name, c.version_of_collectors,
-            srv.id AS srv_id, srv.ip AS srv_ip, srv.user AS srv_user, srv.password AS srv_password, srv.basic_path AS srv_basic_path, -- <-- Added comma here
+            srv.id AS srv_id, srv.ip AS srv_ip, srv.user AS srv_user, srv.password AS srv_password, srv.basic_path AS srv_basic_path,
             s.id AS set_id, s.watching_path, s.folder_in_server_name, s.slow_mode,
             r.id AS rule_id, r.path AS rule_path, r.rule AS rule_value, r.folder_name AS rule_folder, r.copy_file_force
         FROM public.datacollector_collectorcomputer c
         LEFT JOIN public.datacollector_collectorsservers srv 
           ON c.server_id = srv.id
-        JOIN public.datacollector_collectorcomputersettings s 
+        LEFT JOIN public.datacollector_collectorcomputersettings s 
           ON s.collector_computer_id = c.id
-        JOIN public.datacollector_collectorrulesettings r 
+        LEFT JOIN public.datacollector_collectorrulesettings r 
           ON r.collector_computer_settings_id = s.id
         WHERE c.name = %s
     """
@@ -49,25 +50,26 @@ def all_settings_to_machine(cur, comp_name):
             }
 
         s_id = row["set_id"]
-        if s_id not in computers_dict[c_id]["settings"]:
-            computers_dict[c_id]["settings"][s_id] = {
-                "id": s_id,
-                "watching_path": row["watching_path"],
-                "folder_in_server_name": row["folder_in_server_name"],
-                "slow_mode": row["slow_mode"],
-                "rules": [],
-            }
-
-        if row["rule_id"]:
-            computers_dict[c_id]["settings"][s_id]["rules"].append(
-                {
-                    "id": row["rule_id"],
-                    "path": row["rule_path"],
-                    "rule": row["rule_value"],
-                    "folder_name": row["rule_folder"],
-                    "copy_file_force": row["copy_file_force"],
+        if s_id is not None:
+            if s_id not in computers_dict[c_id]["settings"]:
+                computers_dict[c_id]["settings"][s_id] = {
+                    "id": s_id,
+                    "watching_path": row["watching_path"],
+                    "folder_in_server_name": row["folder_in_server_name"],
+                    "slow_mode": row["slow_mode"],
+                    "rules": [],
                 }
-            )
+
+            if row["rule_id"]:
+                computers_dict[c_id]["settings"][s_id]["rules"].append(
+                    {
+                        "id": row["rule_id"],
+                        "path": row["rule_path"],
+                        "rule": row["rule_value"],
+                        "folder_name": row["rule_folder"],
+                        "copy_file_force": row["copy_file_force"],
+                    }
+                )
 
     final_result = []
     for comp in computers_dict.values():
